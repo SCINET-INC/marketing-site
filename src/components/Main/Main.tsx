34521ce730d872c48cb83060439bb857283c61c1
henrybrigham <hhbrigham5@gmail.com>
2021-08-23 15:23:37 -0600

    fixing SCINET spelling

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -18,9 +18,9 @@ export default function Main() {
                   Decentralized Scientific Research
                 </h1>
                 <p>
-                  Sci Net is a decentralized life sciences research and
+                  SCINET is a decentralized life sciences research and
                   investment platform that revolutionizes the way people do and
-                  support science. Powered by blockchain technologies, Sci Net
+                  support science. Powered by blockchain technologies, SCINET
                   allows retail and institutional investors to invest directly
                   in life sciences research and technology with security and
                   authenticity.
