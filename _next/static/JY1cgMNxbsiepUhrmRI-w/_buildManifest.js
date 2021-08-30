2079637107a52b9c43e2797738d983bd6e4facb2
hankbrigham <20171832+hankbrigham@users.noreply.github.com>
2021-08-30 23:17:02 +0000

    Deploying to gh-pages from @ SCINET-INC/marketing-site@0ca34a2222b7958675135e46584b0c79b1438924 🚀

---
+++ _next/static/JY1cgMNxbsiepUhrmRI-w/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/92-d63352e20a9e0a8055c8.js","static/chunks/pages/index-df64c13c3e1365a727ff.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
